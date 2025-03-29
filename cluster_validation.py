c9e55317ca0fb1c7c1c899a527987943910aa14a
Holden Truman <143415887+holden-truman@users.noreply.github.com>
2025-03-29 18:43:34 +0000

    saving

--- cluster_validation.py
+++ cluster_validation.py
@@ -22,7 +22,8 @@ from angle_calc import compute_lensing_angles_astropy
 from angle_calc import _compute_lensing_angles_astropy2
 
 class ClusterLensing:
-    def __init__(self, phot_cat_path, dgamma, z_cl, lensband, ra_c, dec_c):
+    def __init__(self, name, phot_cat_path, dgamma, z_cl, lensband, ra_c, dec_c):
+        self.name = name
         self.phot_cat = fitsio.read(phot_cat_path)
         self.DGAMMA = dgamma
         self.z_cl = z_cl
@@ -36,7 +37,7 @@ class ClusterLensing:
     
     def plot_radius(self):
         plt.scatter(self.phi,self.phi2)
-        plt.title("RXJ2129")
+        plt.title(self.name)
         plt.xlabel(r'$\phi_{old}$ [rad]')
         plt.ylabel(r'$\phi_{new}$ [rad]')
 
@@ -44,10 +45,10 @@ class ClusterLensing:
 
 
 # Example usage:
-rxj2129_lensing = ClusterLensing(phot_cat_path='/sdf/home/h/holden/from_lucie_2024/RXJ2129/RXJ2129_run001_v2_adam.fits',
+rxj2129_lensing = ClusterLensing(name="RXJ2129", phot_cat_path='/sdf/home/h/holden/from_lucie_2024/RXJ2129/RXJ2129_run001_v2_adam.fits',
                            dgamma=2*0.01, z_cl=0.234, lensband='W-C-RC', ra_c=322.41650000, dec_c=0.10588889)
 
-macs1115_lensing = ClusterLensing(phot_cat_path='/sdf/home/h/holden/from_lucie_2024/MACS1115/MACS1115_run002_v2_adam_2.fits',
+macs1115_lensing = ClusterLensing(name="MACS1115", phot_cat_path='/sdf/home/h/holden/from_lucie_2024/MACS1115/MACS1115_run002_v2_adam_2.fits',
                            dgamma=2*0.01, z_cl=0.355, lensband='W-C-RC', ra_c=168.96666667, dec_c=1.49861111)
 
 rxj2129_lensing.plot_radius()
